a719a261b798a7f6ac5257da80594c26cf0ee53e
Al Farisi <codelife.alf@gmail.com>
2023-12-16 18:13:04 +0700

    remove return

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (_, res) {
-  return res.status(200)
+  res.status(200)
     .json({
       'error': false,
       'message': 'Service Running',
--- routes/notFound.js
+++ routes/notFound.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.all('*', function (_, res) {
-  return res.status(404)
+  res.status(404)
     .json({
       'error': true,
       'message': 'Not Found',
